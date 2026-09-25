9b16cd14c5c9ffb4b0e493120ad283a85cb23ebf
Tanveer <tanveernitture9@gmail.com>
2026-09-25 23:27:01 +0530

    Fix duplicate PORT declaration

--- web/server.js
+++ web/server.js
@@ -880,7 +880,7 @@ app.use(
 // START SERVER
 // ======================================================
 
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
